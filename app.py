3141008850a63053b82fd20f0ed04616857680ab
Thodoris Papakonstantinou <dev@tpapak.com>
2025-03-06 11:55:42 +0200

    init conda

--- app.py
+++ app.py
@@ -2263,48 +2263,48 @@ def display_modaldata(cell,rowdata,rowdata_modal):
 
 #####################chatbot#######################################################
 
-from tools.functions_chatbot import *
+# from tools.functions_chatbot import *
 
-@app.callback(
-    Output(component_id="display-conversation", component_property="children"), 
-    Input(component_id="store-conversation", component_property="data")
-)
-def update_display(chat_history):
-    return [
-        render_textbox(x, box="human") if i % 2 == 0 else render_textbox(x, box="AI")
-        for i, x in enumerate(chat_history.split("<split>")[:-1])
-    ]
+# @app.callback(
+    # Output(component_id="display-conversation", component_property="children"), 
+    # Input(component_id="store-conversation", component_property="data")
+# )
+# def update_display(chat_history):
+    # return [
+        # render_textbox(x, box="human") if i % 2 == 0 else render_textbox(x, box="AI")
+        # for i, x in enumerate(chat_history.split("<split>")[:-1])
+    # ]
 
-@app.callback(
-    Output(component_id="user-input", component_property="value"),
-    Input(component_id="submit", component_property="n_clicks"), 
-    Input(component_id="user-input", component_property="n_submit"),
-)
-def clear_input(n_clicks, n_submit):
-    return ""
+# @app.callback(
+    # Output(component_id="user-input", component_property="value"),
+    # Input(component_id="submit", component_property="n_clicks"), 
+    # Input(component_id="user-input", component_property="n_submit"),
+# )
+# def clear_input(n_clicks, n_submit):
+    # return ""
 
-@app.callback(
-    Output(component_id="store-conversation", component_property="data"), 
-    Output(component_id="loading-component", component_property="children"),
-    Input(component_id="submit", component_property="n_clicks"), 
-    Input(component_id="user-input", component_property="n_submit"),
-    State(component_id="user-input", component_property="value"), 
-    State(component_id="store-conversation", component_property="data"),
-)
-def run_chatbot(n_clicks, n_submit, user_input, chat_history):
-    if n_clicks == 0 and n_submit is None:
-        return "", None
+# @app.callback(
+    # Output(component_id="store-conversation", component_property="data"), 
+    # Output(component_id="loading-component", component_property="children"),
+    # Input(component_id="submit", component_property="n_clicks"), 
+    # Input(component_id="user-input", component_property="n_submit"),
+    # State(component_id="user-input", component_property="value"), 
+    # State(component_id="store-conversation", component_property="data"),
+# )
+# def run_chatbot(n_clicks, n_submit, user_input, chat_history):
+    # if n_clicks == 0 and n_submit is None:
+        # return "", None
 
-    if user_input is None or user_input == "":
-        return chat_history, None
+    # if user_input is None or user_input == "":
+        # return chat_history, None
     
-    chat_history += f"Human: {user_input}<split>ChatBot: "
-    # result_ai = conversation.predict(input=user_input)
-    # model_output = result_ai.strip()
-    result_ai = chain.invoke({"text": f"base on {chat_history},{user_input}. Please generate less than 100 words (20-50 wloud be good) and be concise and clear. avoiding the use of bullet points, asterisks (*), or any special formatting."})
-    model_output = result_ai.content
-    chat_history += f"{model_output}<split>"
-    return chat_history, None
+    # chat_history += f"Human: {user_input}<split>ChatBot: "
+    # # result_ai = conversation.predict(input=user_input)
+    # # model_output = result_ai.strip()
+    # result_ai = chain.invoke({"text": f"base on {chat_history},{user_input}. Please generate less than 100 words (20-50 wloud be good) and be concise and clear. avoiding the use of bullet points, asterisks (*), or any special formatting."})
+    # model_output = result_ai.content
+    # chat_history += f"{model_output}<split>"
+    # return chat_history, None
 
 
 
@@ -2323,7 +2323,7 @@ if __name__ == '__main__':
     # app.title = 'NMAstudio' #TODO: title works fine locally, does not on Heroku
     # context = generate_ssl_perm_and_key(cert_name='cert.pem', key_name='key.pem')
     # app.run_server(debug=False, ssl_context=context)
-    app.run_server(port=8080, debug=True) #change port or remove if needed
+    app.run_server(host="192.168.1.217", port=8080, debug=True) #change port or remove if needed
 
 
 
